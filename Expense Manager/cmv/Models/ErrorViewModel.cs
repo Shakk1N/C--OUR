f3cc04d6798abb836c12aa247062a16d7e68a4a0
Shakk1N <159267166+Shakk1N@users.noreply.github.com>
2025-02-20 09:16:41 +0200

    Expense Manager

---
+++ Expense Manager/cmv/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cmv.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
